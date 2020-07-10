48d64af8fab5aa13b1adb7afd20050165222856a
Rustam-Z <zokirovrustam202@gmail.com>
2020-07-10 22:00:09 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+window = tkinter.Tk()
+
+# to rename the title of the window window.title("GUI")
+
+# pack is used to show the object in the window
+
+label = tkinter.Label(window, text="Hello World!").pack()
+
+window.mainloop()
