9e6c4ed54f89e8534c4729c33d824c77e4dc738b
Rustam-Z <zokirovrustam202@gmail.com>
2020-07-11 12:16:14 +0500

    tutorial branch

---
+++ examples/HOWTO.py
@@ -0,0 +1,16 @@
+# PART1: the best way to organize the application
+import tkinter as tk
+
+class MainApplication(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+
+        # <create the rest of your GUI here>
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    MainApplication(root).pack(side="top", fill="both", expand=True)
+    root.mainloop()
+
+# PART2:
---
+++ examples/buttons.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+root.title("GUI")
+
+myButton=Button(root, text="Click me!", ).pack()
+#state=DISABLED
+root.mainloop()
+
