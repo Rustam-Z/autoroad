b9822f0e663d1dd31c3e79bec7e2ed451a933502
Rustam-Z <zokirovrustam202@gmail.com>
2020-07-11 12:19:47 +0500

    myButton

--- examples/buttons.py
+++ examples/buttons.py
@@ -4,6 +4,7 @@ root = Tk()
 root.title("GUI")
 
 myButton=Button(root, text="Click me!", ).pack()
-#state=DISABLED
-root.mainloop()
+# state=DISABLED
+# for size: padx=50, pady=50
 
+root.mainloop()
