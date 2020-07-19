1d8b73003f21bb4142f248d02952fbadab334f70
Rustam-Z <zokirovrustam202@gmail.com>
2020-07-19 23:27:24 +0500

    Scrollbar/Open Notepad/Calendar

--- examples/my_calendar.py
+++ examples/my_calendar.py
@@ -5,6 +5,7 @@ from tkcalendar import *
 
 root = Tk()
 root.title('EightSoft Academy')
+root.geometry("600x400")
 
 cal = Calendar(root, selectmode="day", year=2020, month=5, day=22)
 cal.pack()
@@ -17,6 +18,6 @@ def grab_date():
 my_btn = Button(root, text="Get Date", command=grab_date).pack()
 
 my_label = Label(root, text="")
-
+my_label.pack()
 
 root.mainloop()
