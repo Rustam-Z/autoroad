766c4462c956fedf7a763d1f6d58f776ed94ffd2
Rustam-Z <zokirovrustam202@gmail.com>
2020-07-22 14:49:33 +0500

    Message Boxes

--- project/root.py
+++ project/root.py
@@ -7,48 +7,59 @@ import os
 
 class Root:
     def __init__(self, master):
-        self.my_menu = Menu(master)
-        master.config(menu=self.my_menu)
         self.master = master
 
-        # Create a Menu Item for Teachers
-        self.file_menu = Menu(self.my_menu, tearoff=False)
-        self.my_menu.add_cascade(label="Укитувчи", menu=self.file_menu)
-        self.file_menu.add_command(label="Кошиш", command=self.command_file_new)
-        self.file_menu.add_command(label="Янгилаш", command=self.master.quit)
-        self.file_menu.add_command(label="Учириш", command=self.master.quit)
+        # Creat a Menu Object
+        self.menu_bar = Menu(master)
+        master.config(menu=self.menu_bar)
 
-        # Create a Menu Item Groups
-        self.edit_menu = Menu(self.my_menu, tearoff=False)
-        self.my_menu.add_cascade(label="Гуруҳ", menu=self.edit_menu)
-        self.edit_menu.add_command(label="Кошиш", command=self.command_edit_cut)
-        self.edit_menu.add_command(label="Янгилаш", command=self.master.quit)
-        self.edit_menu.add_command(label="Учириш", command=self.master.quit)
+        # Create a Menu Item for Teachers
+        self.teachers_menu = Menu(self.menu_bar, tearoff=False)
+
+        # Create a Menu Item for Groups
+        self.groups_menu = Menu(self.menu_bar, tearoff=False)
 
         # Create a Menu Item for Students
-        self.edit_menu = Menu(self.my_menu, tearoff=False)
-        self.my_menu.add_cascade(label="Укивчилар", menu=self.edit_menu)
-        self.edit_menu.add_command(label="База", command=self.command_edit_cut)
+        self.students_menu = Menu(self.menu_bar, tearoff=False)
 
-        # Create a Menu Item for Info
-        self.edit_menu = Menu(self.my_menu, tearoff=False)
-        self.my_menu.add_cascade(label="Инфо", menu=self.edit_menu)
-        self.edit_menu.add_command(label="Cut", command=self.command_edit_cut)
-        self.edit_menu.add_separator()
-        self.edit_menu.add_command(label="Question", command=self.master.quit)
+        # Create a Menu Item for Info --EightSoft dev
+        self.info_menu = Menu(self.menu_bar, tearoff=False)
+
+        # Add the cascades for menu bar
+        self.menu_bar.add_cascade(label="Укитувчи", menu=self.teachers_menu)
+        self.menu_bar.add_cascade(label="Гуруҳ", menu=self.groups_menu)
+        self.menu_bar.add_cascade(label="Укивчилар", menu=self.students_menu)
+        self.menu_bar.add_cascade(label="Инфо", menu=self.info_menu)
+
+        # Teachers
+        self.teachers_menu.add_command(label="Кошиш", command=self.command_file_new)
+        self.teachers_menu.add_command(label="Янгилаш", command=self.master.quit)
+        self.teachers_menu.add_command(label="Учириш", command=self.master.quit)
+
+        # Groups
+        self.groups_menu.add_command(label="Кошиш", command=self.command_edit_cut)
+        self.groups_menu.add_command(label="Янгилаш", command=self.master.quit)
+        self.groups_menu.add_command(label="Учириш", command=self.master.quit)
+
+        # Students
+        self.students_menu.add_command(label="База", command=self.command_edit_cut)
+
+        # Info
+        self.info_menu.add_command(label="Дастур хакида маълумот", command=self.command_edit_cut)
+        self.info_menu.add_separator()
 
         # Create some frames
         self.file_new_frame = ttk.Frame(self.master, width=400, height=400)
         self.edit_cut_frame = ttk.Frame(self.master, width=400, height=400)
 
-        # Showing the list of groups
-        files_list = os.listdir('groups')
-        files_list_box = Listbox(self.master)
-        files_list_box.pack(side=TOP, anchor=NW)
-        # THE ITEMS INSERTED WITH A LOOP
-        for item in files_list:
-            item = item[:-5]
-            files_list_box.insert(END, item)
+        # # Showing the list of groups
+        # files_list = os.listdir('groups')
+        # files_list_box = Listbox(self.master)
+        # files_list_box.pack(side=TOP, anchor=NW)
+        # # THE ITEMS INSERTED WITH A LOOP
+        # for item in files_list:
+        #     item = item[:-5]
+        #     files_list_box.insert(END, item)
 
         # files_list = os.listdir('groups')
         # item_count = 0
@@ -91,11 +102,10 @@ class Root:
 def main():
     root = ThemedTk(theme="breeze")
     root.geometry("500x500+250+100")
+    root.title("AutoRoad Excel")
     app = Root(root)
     root.mainloop()
 
 
 if __name__ == '__main__':
     main()
-
-
